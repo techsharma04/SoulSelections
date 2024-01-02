a37cf02c31222abc767cc5335642f213d33763de
techsharma04 <techsharma04@outlook.com>
2024-01-01 22:14:35 -0500

    First Step Done

---
+++ src/api/EndPoints.js
@@ -0,0 +1,13 @@
+import Constants from "./Constants";
+
+const Endpoints = {
+    CATEGORY_ALL_URL: `${Constants.BASE_URL}products/categories`,
+    PRODUCT_ALL: `${Constants.BASE_URL}products/`,
+    SINGLE_PRODUCT: `${Constants.BASE_URL}products/`,
+    CATEGORY_BASED_PRODUCTS: `${Constants.BASE_URL}products/category/`,
+    LOGIN_URL: `https://soulselections.onrender.com/posts`,
+    REGISTER_URL: `https://soulselections.onrender.com/posts`,
+    CART_URL: `https://soulselectionscart.onrender.com/posts`
+}
+
+export default Endpoints; 
